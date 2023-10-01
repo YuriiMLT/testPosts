2f9af2f69b8045ee35d15830c2c0a45d94c16747
YuriiMLT <yurii0buriak@gmail.com>
2023-10-01 15:25:51 +0200

    migration for the posts table

--- src/migrations/1696164353448-MyMig.ts
+++ src/migrations/1696164353448-MyMig.ts
@@ -1,11 +1,54 @@
-import { MigrationInterface, QueryRunner } from "typeorm"
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
-export class MyMig1696164353448 implements MigrationInterface {
+export class CreatePostsTimestamp implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: "posts",
+            columns: [
+                {
+                    name: "id",
+                    type: "int",
+                    isPrimary: true,
+                    isGenerated: true,
+                    generationStrategy: 'increment'
+                },
+                {
+                    name: "title",
+                    type: "varchar",
+                    isNullable: false
+                },
+                {
+                    name: "content",
+                    type: "text",
+                    isNullable: false
+                },
+                {
+                    name: "short_description",
+                    type: "text",
+                    isNullable: false
+                },
+                {
+                    name: "img_url",
+                    type: "varchar",
+                    isNullable: true
+                },
+                {
+                    name: "created_at",
+                    type: "timestamp",
+                    default: "CURRENT_TIMESTAMP"
+                },
+                {
+                    name: "updated_at",
+                    type: "timestamp",
+                    default: "CURRENT_TIMESTAMP",
+                    onUpdate: "CURRENT_TIMESTAMP"
+                }
+            ]
+        }));
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("posts");
     }
-
 }
